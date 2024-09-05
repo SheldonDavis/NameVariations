1f3efb887129bb6b3be8a3aa8766a2e0deebba68
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-09-05 09:43:31 -0400

    handle blank names, clear unnecessary starting array value, error handling,

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,28 @@ function App() {
   const [LastNameFirst, setLastNameFirst] = useState<boolean>(false);
 
   const [FirstNames, setFirstNames] = useState<Array<string>>(['']);
-  const [MiddleNames, setMiddleNames] = useState<Array<string>>(['']);
+  const [NickNames, setNickNames] = useState<Array<string>>([]);
+  const [MiddleNames, setMiddleNames] = useState<Array<string>>([]);
   const [LastNames, setLastNames] = useState<Array<string>>(['']);
-  const [NickNames, setNickNames] = useState<Array<string>>(['']);
-  const [SirNames, setSirNames] = useState<Array<string>>(['']);
+  const [SirNames, setSirNames] = useState<Array<string>>([]);
 
   const [Variations, setVariations] = useState<Array<string>>([]);
 
   function GenerateNameVariations() {
     const tempArray: Array<string> = [];
+    let missingRequired: boolean = false;
     LastNames.forEach((lName) => {
+      if (!lName) {
+        tempArray.push(`No Names variations can be created without a last name`);
+        missingRequired = true;
+        return;
+      }
       FirstNames.forEach((fName) => {
+        if (!fName) {
+          tempArray.push(`No Names variations can be created without a first name`);
+          missingRequired = true;
+          return;
+        }
         tempArray.push(`${fName} ${lName}`);
         tempArray.push(`${fName.charAt(0)} ${lName}`);
         if (LastNameFirst) {
@@ -28,6 +39,9 @@ function App() {
         }
 
         MiddleNames.forEach((mName) => {
+          if (!mName) {
+            return;
+          }
           tempArray.push(`${fName} ${mName} ${lName}`);
           tempArray.push(`${fName} ${mName.charAt(0)} ${lName}`);
           tempArray.push(`${fName.charAt(0)} ${mName} ${lName}`);
@@ -39,10 +53,16 @@ function App() {
         });
 
         SirNames.forEach((sName) => {
+          if (!sName) {
+            return;
+          }
           tempArray.push(`${fName} ${lName} ${sName}`);
           tempArray.push(`${fName.charAt(0)} ${lName} ${sName}`);
 
           MiddleNames.forEach((mName) => {
+            if (!mName) {
+              return;
+            }
             tempArray.push(`${fName} ${mName} ${lName} ${sName}`);
             tempArray.push(`${fName.charAt(0)} ${mName.charAt(0)} ${lName} ${sName}`);
             tempArray.push(`${fName} ${mName.charAt(0)} ${lName} ${sName}`);
@@ -52,20 +72,32 @@ function App() {
       });
 
       NickNames.forEach((nName) => {
+        if (!nName || missingRequired) {
+          return;
+        }
         tempArray.push(`${nName} ${lName}`);
         if (LastNameFirst) {
           tempArray.push(`${lName}, ${nName}`);
         }
 
         MiddleNames.forEach((mName) => {
+          if (!mName) {
+            return;
+          }
           tempArray.push(`${nName} ${mName} ${lName}`);
           tempArray.push(`${nName} ${mName.charAt(0)} ${lName}`);
         });
 
         SirNames.forEach((sName) => {
+          if (!sName) {
+            return;
+          }
           tempArray.push(`${nName} ${lName} ${sName}`);
 
           MiddleNames.forEach((mName) => {
+            if (!mName) {
+              return;
+            }
             tempArray.push(`${nName} ${mName} ${lName} ${sName}`);
             tempArray.push(`${nName} ${mName.charAt(0)} ${lName} ${sName}`);
           });
@@ -95,7 +127,7 @@ function App() {
           Borrower is signing last name first
         </p>
       </div>
-      <div className={`flexRow centered`}>
+      <div className={`flexRow centered wrap`}>
         <span className={`flexCol`}>
           <TextBoxList titleText={`First Name`} Names={FirstNames} setNames={setFirstNames} />
         </span>
@@ -114,18 +146,17 @@ function App() {
       </div>
 
       <div id={`outputArea`} className={`flexRow centered`}>
-        <button onClick={GenerateNameVariations}>Generate Name Variations</button>
+        <button onClick={GenerateNameVariations} className={`greenBtn`}>
+          Generate Name Variations
+        </button>
       </div>
       <div className={`flexRow centered wrap`}>
-        {Variations.length > 0 && (
-          <>
-            {Variations?.map((Name: string, i: number) => (
-              <>
-                <span key={i}>{Name};</span>
-              </>
-            ))}
-          </>
-        )}
+        {Variations.length > 0 &&
+          Variations?.map((Name: string, i: number) => (
+            <>
+              <span key={i}>{Name};</span>
+            </>
+          ))}
       </div>
     </>
   );
