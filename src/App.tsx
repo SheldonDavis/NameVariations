cb410bb5348402e4775fd5c4f8c4023879edfb05
Sheldon Davis <sheldon.davis1515@gmail.com>
2024-09-19 09:08:07 -0400

    added a space after each variation output's semicolon

--- src/App.tsx
+++ src/App.tsx
@@ -170,7 +170,7 @@ function App() {
           Generate Name Variations
         </button>
       </div>
-      <div className={`flexRow centered wrap`}>{Variations.length > 0 && Variations?.map((Name: string) => <>{Name};</>)}</div>
+      <div className={`flexRow centered wrap`}>{Variations.length > 0 && Variations?.map((Name: string) => <>{Name}; </>)}</div>
     </>
   );
 }
